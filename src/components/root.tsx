ffd56d47fcb4183bc1db31a98e39530a5928d02f
PetrusMaximus0 <45740112+petrusmaximus0@users.noreply.github.com>
2025-02-24 16:55:03 +0000

    refactor: Remove unnecessary component and route.

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -1,9 +1,11 @@
-import { Outlet } from 'react-router-dom';
+import {Outlet, useNavigation} from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Icon } from '@mdi/react';
 import { mdiRobotIndustrialOutline } from '@mdi/js';
+import SideBar from "./sideBar.tsx";
 
 export default function Root() {
+	const {state} = useNavigation();
 	return (
 		<div className="h-screen max-w-screen-2xl px-1 mx-auto py-12 sm:px-6 lg:px-10 gap-8 grid grid-rows-[auto_1fr_auto]">
 			<header className="mx-auto">
@@ -14,7 +16,16 @@ export default function Root() {
 					</h1>
 				</Link>
 			</header>
-			<Outlet />
+			{ state === "idle" &&
+				<div className="grid grid-rows-[auto_auto] gap-8 lg:grid-rows-1 lg:grid-cols-[1fr_minmax(auto,_320px)]">
+					<main>
+						<Outlet/>
+					</main>
+					<aside>
+						<SideBar/>
+					</aside>
+				</div> || <p className={"flex h-full w-full items-center justify-center text-2xl"}> Loading...  </p>
+			}
 			<footer className="text-center py-10 font-bold text-lg">
 				<Link to="/"> THE BLOG PAGE </Link>
 			</footer>
